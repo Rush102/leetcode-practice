641dbfd29f776d39e0f4b12b3e1d856ac9ca3f6a
Rush102 <10044104+Rush102@users.noreply.github.com>
2025-03-11 19:14:04 +0800

    test

---
+++ Top-Interview-150/Linked-Lists/leetcode_82_remove_duplicates.cpp
@@ -0,0 +1,20 @@
+#include <unordered_set>
+
+ struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+    };
+ 
+ class Solution {
+    public:
+        ListNode* deleteDuplicates(ListNode* head) {
+            ListNode* start = head;
+            unordered_set <int> usedset;
+            while(head != nullptr){
+                if(usedset)
+            }
+        }
+    };
